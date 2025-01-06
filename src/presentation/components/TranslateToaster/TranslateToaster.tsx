8dbd7ba62f02e6f4f029008a84b7496422b43b73
Juan Pablo Leon <juanleon8581@gmail.com>
2025-01-06 12:23:13 -0500

    feat: enhance TranslateToaster with local storage integration for translation activation

--- src/presentation/components/TranslateToaster/TranslateToaster.tsx
+++ src/presentation/components/TranslateToaster/TranslateToaster.tsx
@@ -1,12 +1,16 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { toast } from "@/hooks/use-toast";
 import { useTextSelection } from "@/hooks/useTextSelection";
 import { useTranslation } from "@/hooks/useTranslation";
+import { useLocalStorage } from "@/hooks";
+import { GotoTranslateData } from "..";
 
 interface TranslateToasterProps {
   targetLanguage?: string;
 }
+
+const STORAGE_KEY = "gotoTranslateActive";
 
 export const TranslateToaster = ({
   targetLanguage = "en",
@@ -16,25 +20,41 @@ export const TranslateToaster = ({
     text: selectedText,
     targetLang: targetLanguage,
   });
+  const [customStorage] = useLocalStorage();
+
+  const getStorageValue = useCallback(async () => {
+    const data = (await customStorage({
+      op: "get",
+      key: STORAGE_KEY,
+    })) as GotoTranslateData;
+
+    return data?.gotoTranslateActive || data;
+  }, [customStorage]);
 
   useEffect(() => {
-    if (error) {
-      toast({
-        title: "Translation Error",
-        description: error.message,
-        variant: "destructive",
-        duration: 5000,
-      });
-      return;
-    }
+    const translateText = async () => {
+      const gotoIsActive = await getStorageValue();
+      const isActive = gotoIsActive === "true";
+      if (!isActive) return;
+      if (error) {
+        toast({
+          title: "Translation Error",
+          description: error.message,
+          variant: "destructive",
+          duration: 5000,
+        });
+        return;
+      }
 
-    if (translation) {
-      toast({
-        title: translation,
-        duration: 15000,
-      });
-    }
-  }, [translation, error]);
+      if (translation) {
+        toast({
+          title: translation,
+          duration: 15000,
+        });
+      }
+    };
+    translateText();
+  }, [translation, error, getStorageValue]);
 
   return <Toaster />;
 };
