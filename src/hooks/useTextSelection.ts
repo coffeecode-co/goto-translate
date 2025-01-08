a64c0e9f09503fe0358a97e8888de3963ccf8ed7
jpleon <juanleon8581@gmail.com>
2025-01-07 20:34:32 -0500

    feat: enhance useTextSelection hook to capture selection change events

--- src/hooks/useTextSelection.ts
+++ src/hooks/useTextSelection.ts
@@ -1,19 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { useTranslateStore } from "./";
 
 export const useTextSelection = () => {
+  const [selectEvent, setSelectEvent] = useState<Event>(new Event(""));
   const setSelectedText = useTranslateStore((state) => state.setSelectedText);
 
   useEffect(() => {
-    const handleSelection = () => {
+    const handleSelection = (e: Event) => {
       const text = window.getSelection()?.toString().trim();
 
       setSelectedText(text ?? "");
+      setSelectEvent(e);
     };
 
     document.addEventListener("selectionchange", handleSelection);
     return () =>
       document.removeEventListener("selectionchange", handleSelection);
   }, [setSelectedText]);
+
+  return [selectEvent];
 };
