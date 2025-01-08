d2b8691f9a3cbf5f726b39ab4521d39c3f6428d1
jpleon <juanleon8581@gmail.com>
2025-01-07 20:34:45 -0500

    feat: export useHotkeyBind hook from index

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,5 +2,6 @@ export { useLocalStorage } from "./useLocalStorage";
 export { useTextSelection } from "./useTextSelection";
 export { useTranslation } from "./useTranslation";
 export { useTranslateStore } from "./useStore";
+export { useHotkeyBind } from "./useHotkeyBind";
 
 export type { UseLocalStorageProps } from "./useLocalStorage";
