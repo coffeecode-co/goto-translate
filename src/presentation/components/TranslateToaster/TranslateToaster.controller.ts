1e352afe6c63afe6b8eea4724dbc4ece612fdb29
Juan Pablo Leon <juanleon8581@gmail.com>
2025-01-14 12:28:05 -0500

    translateToaster refactoring to always use keyboard hotkey

--- src/presentation/components/TranslateToaster/TranslateToaster.controller.ts
+++ src/presentation/components/TranslateToaster/TranslateToaster.controller.ts
@@ -1,24 +1,40 @@
+import { toast } from "@/hooks/use-toast";
+
 interface HandleHotkeyProps {
   eventTarget: HTMLInputElement;
   getEditableElements: (
     element: HTMLInputElement
   ) => HTMLInputElement | undefined;
+  translateActive: () => Promise<string>;
   doTranslate: () => Promise<string | undefined>;
 }
+
+const DEFAULT_TOAST_DURATION = 15000;
+
+const showTranslationToast = (text: string) => {
+  const theToast = toast({
+    title: text,
+    duration: DEFAULT_TOAST_DURATION,
+  });
+  if (!text) theToast.dismiss();
+};
 
 export const handleHotkey = async ({
   eventTarget,
   getEditableElements,
+  translateActive,
   doTranslate,
 }: HandleHotkeyProps) => {
   try {
     const element = getEditableElements(eventTarget);
     const textSelected = document.getSelection()?.toString().trim();
+    const translateActived = await translateActive();
+    if (translateActived === "false") return;
     const text = await doTranslate();
     if (!textSelected) return;
-    if (!element) return;
     if (!text) return;
-    document.execCommand("insertText", false, text);
+    if (element) return document.execCommand("insertText", false, text);
+    showTranslationToast(text);
   } catch (error) {
     console.error("Error handling hotkey:", error);
   }
--- src/presentation/components/TranslateToaster/TranslateToaster.tsx
+++ src/presentation/components/TranslateToaster/TranslateToaster.tsx
@@ -1,9 +1,8 @@
-import { useCallback, useEffect } from "react";
+import { useCallback } from "react";
 import { Toaster } from "@/components/ui/toaster";
-import { toast } from "@/hooks/use-toast";
+
 import {
   useLocalStorage,
-  useTranslateStore,
   useTranslation,
   useTextSelection,
   useHotkeyBind,
@@ -13,44 +12,24 @@ import { GLOBAL_STRINGS } from "@/config";
 
 import { handleHotkey } from "./TranslateToaster.controller";
 
-import type { GotoTranslateData } from "..";
-
 const { TRANSLATE_ACTIVE } = GLOBAL_STRINGS.LOCAL_STORAGE_KEY;
-const DEFAULT_TOAST_DURATION = 15000;
-const ERROR_TOAST_DURATION = 5000;
 
 export const TranslateToaster = () => {
   const [selectEvent] = useTextSelection();
-  const { translatedText } = useTranslateStore();
-  const { error: errorTranslate, translateText } = useTranslation({
+  const { translateText } = useTranslation({
     nativeLangKey: GLOBAL_STRINGS.LOCAL_STORAGE_KEY.NATIVE_LENGUAGE,
     targetLangKey: GLOBAL_STRINGS.LOCAL_STORAGE_KEY.TARGET_LENGUAGE,
   });
   const [customStorage] = useLocalStorage();
 
-  const getStorageValue = useCallback(async (): Promise<
-    string | GotoTranslateData
-  > => {
-    const data = (await customStorage({
+  const getStorageValue = useCallback(async (): Promise<string> => {
+    const data = await customStorage({
       op: "get",
       key: TRANSLATE_ACTIVE,
-    })) as GotoTranslateData;
+    });
 
-    return data?.gotoTranslateActive || data;
+    return data ?? "false";
   }, [customStorage]);
-
-  const showTranslationToast = useCallback(
-    (text: string, isError = false) => {
-      const theToast = toast({
-        title: isError ? "Translation Error" : text,
-        description: isError ? errorTranslate?.message : undefined,
-        variant: isError ? "destructive" : undefined,
-        duration: isError ? ERROR_TOAST_DURATION : DEFAULT_TOAST_DURATION,
-      });
-      if (!text) theToast.dismiss();
-    },
-    [errorTranslate?.message]
-  );
 
   useHotkeyBind([
     {
@@ -59,36 +38,12 @@ export const TranslateToaster = () => {
         handleHotkey({
           eventTarget: selectEvent.target as HTMLInputElement,
           getEditableElements,
+          translateActive: getStorageValue,
           doTranslate: translateText,
         });
       },
     },
   ]);
-
-  useEffect(() => {
-    const translateText = async () => {
-      try {
-        const gotoIsActive = await getStorageValue();
-        if (gotoIsActive !== "true") return;
-
-        if (errorTranslate) {
-          showTranslationToast("", true);
-          return;
-        }
-
-        showTranslationToast(translatedText);
-      } catch (error) {
-        console.warn("Error in translation process:", error);
-      }
-    };
-
-    if (
-      (translatedText !== null && translatedText !== undefined) ||
-      errorTranslate
-    ) {
-      translateText();
-    }
-  }, [translatedText, errorTranslate, getStorageValue, showTranslationToast]);
 
   return <Toaster />;
 };
